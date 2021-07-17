53dae9cc940b31126d5b6c9b2cdb3744f736c797
Jessica PAQUET <jessica-paquet@live.fr>
2021-07-17 04:19:12 +0200

    articles

--- client/index.js
+++ client/index.js
@@ -1,2 +1,2 @@
 console.log("test")
-fetch("http://localhost:3000/api/cameras").then((r) => r.json()).then((r) => console.log(r))
+fetch("http://localhost:3000/api/teddies").then((r) => r.json()).then((r) => console.log(r))
